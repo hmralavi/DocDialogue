61c9dbf5958cbe3f4cbd784a537514d8ee219b7d
hmralavi <hmralavi@gmail.com>
2024-05-26 15:29:24 +0330

    github link added

--- src/main.py
+++ src/main.py
@@ -125,7 +125,7 @@ def insert_message_stream(stream):
 
 
 def stylized_msg(role: str, msg: str):
-    return f"""<div class="message-{role}">{msg}</div>"""
+    return f"<div class='message-{role}'>{msg}</div>"
 
 
 def display_all_messages():
@@ -184,10 +184,12 @@ def get_prompt_template():
         [
             (
                 "human",
-                """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+                """
+                You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
                 Question: {question}
                 Context: {context}
-                Answer:""",
+                Answer:
+                """,
             )
         ]
     )
@@ -207,10 +209,12 @@ def get_prompt_template():
     )
 
     prompt3 = PromptTemplate.from_template(
-        """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+        """
+        You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
         Question: {question}
         Context: {context}
-        Answer:"""
+        Answer:
+        """
     )
 
     return prompt2
@@ -225,32 +229,28 @@ def main():
         page_icon="🤖",
     )
 
+    # set page style
+    with open("./src/styles.css", "r") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
     # set page title
     st.markdown(
-        """<span style='font-size: 48px;font-weight: bold;'>🤖 DocDialogue</span>
-        <span style='font-size: 20px;font-weight: normal;margin-left: 10px;'>Chat with your document!</span>""",
+        """
+        <span style='font-size: 48px;font-weight: bold;'>🤖 DocDialogue</span>
+        <span style='font-size: 20px;font-weight: normal;margin-left: 10px;'>Chat with your document!</span>
+        """,
         unsafe_allow_html=True,
     )
 
-    st.markdown(
+    # add github link
+    st.sidebar.markdown(
         """
-        <style>
-        .stChatMessage {
-            background-color: transparent;
-        }
-        .message-assistant {
-            background-color: rgba(217, 167, 0, 0.3);
-            padding: 10px;
-            border-radius: 5px;
-            margin-bottom: 10px;
-        }
-        .message-user {
-            background-color: rgba(150, 150, 150, 0.3);
-            padding: 10px;
-            border-radius: 5px;
-            margin-bottom: 10px;
-        }
-        </style>
+        <a href="https://github.com/hmralavi/DocDialogue" class="github-link" target="_blank">
+            <svg height="24" width="24" aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" fill="white">
+                <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.58.82-2.13-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.55.82 1.26.82 2.13 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.19 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+            </svg>
+            GitHub
+        </a>
         """,
         unsafe_allow_html=True,
     )
@@ -273,7 +273,7 @@ def main():
         st.session_state.rag_chain = get_chain()
 
     st.sidebar.markdown(
-        "<span style='font-size: 20px;font-weight: bold;'>Select LLM Model:</span>",
+        "<span style='font-size: 36px;font-weight: bold;'>Options:</span>",
         unsafe_allow_html=True,
     )
 
@@ -283,7 +283,6 @@ def main():
         index=0,
         on_change=llm_model_changed_callback,
         key="llm_model",
-        label_visibility="hidden",
     )
 
     if "messages" not in st.session_state:
